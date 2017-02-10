77a4aae5295fda2e3271e65eb92083518f225780
sergiy <sich-sergjj@rambler.ru>
2017-02-10 13:48:28 +0200

    models are created

--- library/models.py
+++ library/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    class Meta:
+        db_table = 'Author'
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    class Meta:
+        db_table = 'Book'
+    title = models.CharField(max_length=50)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.title
+
