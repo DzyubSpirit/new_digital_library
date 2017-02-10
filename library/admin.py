4af502123182b77c7eac8df3608dc334bf9795fc
sergiy <sich-sergjj@rambler.ru>
2017-02-10 13:57:42 +0200

    models are registered

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from library.models import Author, Book
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    filter_horizontal = ('authors',)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
